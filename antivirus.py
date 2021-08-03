a89951bc4e653063a68c9a61e35a588a5f2452db
SoA-Lee <llsa0927@naver.com>
2021-08-03 23:25:51 +0900

    [mod&upload]entire

--- antivirus.py
+++ antivirus.py
@@ -2,14 +2,46 @@
 import hashlib
 import os
 import sys
+import zlib
+import StringIO
 
 VirusDB = [] # 악성코드 패턴은 모두 virus.db에 존재함
 vdb = [] # 가공된 악성코드 DB가 저장된다.
 vsize = [] # 악성코드의 파일 크기만 저장한다.
 
+# KMD 파일을 복호화한다.
+def DecodeKMD(fname) :
+    try :
+        fp = open(fname,'rb') # 복호화 대상 파일을 연다.
+        buf = fp.read()
+        fp.close()
+
+        buf2 = buf[:-32] # 암호화 내용을 분리한다.
+        fmd5 = buf[-32:] #md5를 분리한다.
+
+        f =buf2
+        for i in range(3) : # 암호화 내용의 md5를 구한다.
+            md5 = hashlib.md5()
+            md5.update(f)
+            f = md5.hexdigest()
+
+        if f != fmd5 : # 위 결과와 파일에서 분리된 md5가 같은가?
+            raise SystemError
+        buf3 = ''
+        for c in buf2[4:] :
+            buf3 +=chr(ord(c)^0xFF)
+
+        buf4 = zlib.decompress(buf3) # 압축을 해제한다.
+        return buf4 # 성공했다면 복호화된 내용을 리턴한다.
+    except :
+        pass
+
+    return None # 오류가 있다면 None을 리턴한다.
+
 # virus.db 파일에서 악성코드 패턴을 읽는다.
 def LoadVirusDB() :
-    fp = open('virus.db','rb') # 악성코드 패턴을 연다.
+    buf = DecodeKMD('virus.kmd') # 악성코드 파일을 복호화한다.
+    fp = StringIO.StringIO(buf)
 
     while True :
         line = fp.readline() # 악성코드 패턴을 한 줄 읽는다.
@@ -18,18 +50,18 @@ def LoadVirusDB() :
         VirusDB.append(line) # 악성코드 패턴을 VirusDB에 추가한다.
 
     fp.close() # 악성코드 패턴 파일을 닫는다.
- 
+
 # VirusDB를 가공하여 VDB에 저장한다.
-def MakeVirusDB() :
-    for pattern in VirusDB :
+def MakeVirusDB():
+    for pattern in VirusDB:
         t = []
-        v = pattern.split(':') # 세미콜론을 기준으로 자른다.
-        t.append(v[1]) # MD5 해시를 저장한다.
-        t.append(v[2]) # 악성코드 이름을 저장한다.
-        vdb.append(t) # 최종 vdb에 저장한다.
-        
-        size = int(v[0]) # 악성코드 파일 크기
-        if vsize.count(size) == 0 : #이미 해당 크기가 등록되었나?
+        v = pattern.split(':')
+        t.append(v[1])  # 세미콜론을 기준으로 자른다.
+        t.append(v[2])
+        vdb.append(t)
+
+        size = int(v[0])  # 악성코드 파일 크기
+        if vsize.count(size) == 0:
             vsize.append(size)
         
 # 악성코드를 검사한다.
@@ -40,7 +72,8 @@ def SearchVDB(fmd5):
     return False, '' # 악성코드가 발견되지 않음
 
 if __name__ == '__main__' :
-    MakeVirusDB() # 악성코드 DB를 가공한다.
+    LoadVirusDB()
+    MakeVirusDB()
     
     # 커맨드라인으로 악성코드를 검사할 수 있게 한다.
     # 커맨드라인의 입력 방식을 체크한다.
---
+++ kmake.py
@@ -0,0 +1,43 @@
+# -*-coding:utf-8 -*-
+import hashlib
+import os
+import sys
+import zlib
+
+# 주어진 파일을 암호화한다.
+def  main() :
+    if len(sys.argv) !=2 :
+        print 'Usage : kmake.py [file]'
+        return
+    
+    fname = sys.argv[1] # 암호화 대상 파일
+    tname = fname
+    
+    fp = open(tname, 'rb')
+    buf = fp.read()
+    fp.close()
+    
+    buf2=zlib.compress(buf) # 대상 파일 내용을 압축한다.
+    
+    buf3 = ''
+    for c in buf2:
+        buf3 +=chr(ord(c)^0xFF)
+        
+    buf4 = 'KAVM'+buf3 # 헤더 생성
+    
+    f = buf4
+    for i in range(3):
+        md5 = hashlib.md5()
+        md5.update(f)
+        f=md5.hexdigest()
+        
+    buf4+=f # md5를 암호화된 내용 뒤에 추가한다.
+    kmd_name=fname.split('.')[0]+'.kmd'
+    fp=open(kmd_name,'wb') # 확장자로 암호 파일을 만든다.
+    fp.write(buf4)
+    fp.close()
+    
+    print '%s -> %s' %(fname, kmd_name)
+
+if __name__ == '__main__' :
+    main()
