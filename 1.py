df3890170261b0395de475fda57e5b05320d7687
SoA-Lee <llsa0927@naver.com>
2021-08-03 22:00:20 +0900

    [mod]MyFirstVaccine

--- 1.py
+++
@@ -1,8 +0,0 @@
-fp = open ('eicar.txt','rt') #바이너리 모드로 읽기
-fbuf = fp.read()
-if fbuf[0:3] == 'X5O' :
-    print('Virus')
-    os.remove('eicar.txt') #파일을 삭제해서 치료
-else:
-    print('No Virus')
-fp.close()
