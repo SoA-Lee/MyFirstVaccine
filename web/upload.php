c563ae84fdaae493c101f6bc142cbb7493a0fbc0
dalgorithm <67897318+SoA-Lee@users.noreply.github.com>
2021-08-23 14:47:41 +0900

    [mod]upload.php

--- web/upload.php
+++ web/upload.php
@@ -52,20 +52,53 @@ function formSubmit(f) {
 
 </script>
 
+<style>
+        #btn1{
+                border-top-left-radius: 5px;
+                border-bottom-left-radius: 5px;
+                border-top-right-radius: 5px;
+                border-bottom-right-radius: 5px;
+                margin-right:-4px;
+                padding:5px;
+        }
+        #btn2{
+                border-top-left-radius: 5px;
+                border-bottom-left-radius: 5px;
+                border-top-right-radius: 5px;
+                border-bottom-right-radius: 5px;
+                margin-left:-3px;
+                color: #6495ED;
+                border: 3px solid #6495ED;
+                padding: 5px;
+        }
+        #hr{
+                height: 2px;
+                background: #bbb;
+                background-image: -webkit-linear-gradient(left, #eee, #777, #eee);
+                background-image: -moz-linear-gradient(left, #eee, #777, #eee);
+                background-image: -ms-linear-gradient(left, #eee, #777, #eee);
+                background-image: -o-linear-gradient(left, #eee, #777, #eee);
+        }
+</style>
 
-
+<div style="text-align: center; position: absolute; top:300px; left: 40%;" >
 <form name="uploadForm" id="uploadForm" method="post" action="upload_process.php"
 
       enctype="multipart/form-data" onsubmit="return formSubmit(this);">
 
     <div>
-
-        <label for="upfile">첨부파일</label>
-
-        <input type="file" name="upfile" id="upfile" />
-
+        <img src="./image/koren_image.jfif" alt="My Image" style="width: 50%; height: auto;"> </p>
+        <label for="upfile" style="font-size: 30px; color: #6495ED; font-style: italic;" >첨부파일을 넣어주세요.
+        </label>
+        </p>
+        </p>
+        <hr style="border: solid 2px grey;">
+        </p>
+        </p>
+        <input id="btn1" type="file" name="upfile" id="upfile" color="blue" />
+        </p>
+        <input id="btn2"  type="submit" value="upload" />
     </div>
 
-    <input type="submit" value="업로드" />
-
 </form>
+</div>
